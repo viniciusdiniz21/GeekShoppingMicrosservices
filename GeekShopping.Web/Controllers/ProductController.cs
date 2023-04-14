68587be614062de5ac2d966121ebea9a57b37fb8
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-04-14 14:33:11 -0300

    Finalizando interface de produtos

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
@@ -16,6 +17,57 @@ namespace GeekShopping.Web.Controllers
         {
             var products = await _productService.FindAllProducts();
             return View(products);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(model);
+                if(response != null) return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var product = await _productService.FindProductById(id);
+            if (product != null) return View(product);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+                if (response != null) return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var product = await _productService.FindProductById(id);
+            if (product != null) return View(product);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+            
+            var response = await _productService.DeleteProduct(model.Id);
+            if (response != null) return RedirectToAction(nameof(ProductIndex));
+            
+            return View(model);
         }
     }
 }
--- GeekShopping.Web/Services/ProductService.cs
+++ GeekShopping.Web/Services/ProductService.cs
@@ -1,32 +1,21 @@
 ﻿using GeekShopping.Web.Models;
 using GeekShopping.Web.Services.IServices;
 using GeekShopping.Web.Utils;
-using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace GeekShopping.Web.Services
 {
     public class ProductService : IProductService
     {
         private readonly HttpClient _client;
-        public const string BasePath = "api/v1/product";
+        public const string BasePath = "api/product";
 
         public ProductService(HttpClient client)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
-        }
-
-        public async Task<ProductModel> CreateProduct([FromBody] ProductModel model)
-        {
-            var response = await _client.PostAsJson(BasePath, model);
-            if (response.IsSuccessStatusCode)
-                return await response.ReadContentAs<ProductModel>();
-            else throw new Exception("Something went wrong.");
-        }
-
-        public async Task<bool> DeleteProduct(long id)
-        {
-            var response = await _client.DeleteAsync($"{BasePath}/{id}");
-            return await response.ReadContentAs<bool>();
         }
 
         public async Task<IEnumerable<ProductModel>> FindAllProducts()
@@ -41,12 +30,27 @@ namespace GeekShopping.Web.Services
             return await response.ReadContentAs<ProductModel>();
         }
 
-        public async Task<ProductModel> UpdateProduct([FromBody] ProductModel model)
+        public async Task<ProductModel> CreateProduct(ProductModel model)
+        {
+            var response = await _client.PostAsJson(BasePath, model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+            else throw new Exception("Something went wrong when calling API");
+        }
+        public async Task<ProductModel> UpdateProduct(ProductModel model)
         {
             var response = await _client.PutAsJson(BasePath, model);
             if (response.IsSuccessStatusCode)
                 return await response.ReadContentAs<ProductModel>();
-            else throw new Exception("Something went wrong.");
+            else throw new Exception("Something went wrong when calling API");
+        }
+
+        public async Task<bool> DeleteProduct(long id)
+        {
+            var response = await _client.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+            else throw new Exception("Something went wrong when calling API");
         }
     }
 }
